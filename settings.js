cc7e697b412766e4b1276433b4eb0f90da20e514
Fakenumbr <163015441+Fakenumbr@users.noreply.github.com>
2024-04-01 11:24:31 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,27 +21,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.owner = ['919846201004'] //ur owner number
+global.ownernomer = "919846201004" //ur owner number2
+global.ownername = "Arshith" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.socialm = "GitHub: CgArshith" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
 global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
+global.ownernumber = '919846201004'
+global.ownername = 'Arshith'
+global.ownerNumber = ["919846201004@s.whatsapp.net"]
 global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
+global.websitex = "https://www.instagram.com/arsh_th_?igsh=OGQ5ZDc2ODk2ZA=="
+global.wagc = "https://chat.whatsapp.com/BimhDs7A75W79XbOY4D0iW"
 global.themeemoji = '🪀'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
+global.packname = "Kaliyadi makkalee"
+global.author = "𝛥𝑅𝑆𝛨𝛪𝑇𝛨\n\n"
+global.creator = "919846201004@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
